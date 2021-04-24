2700a06877e05af17d6758d829bfe04bb502b41a
Maxim Vostretsov <47358715+Moxi43@users.noreply.github.com>
2021-04-24 16:05:33 +0400

    This version is final. I hope so...

--- vinci.cpp
+++ vinci.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-/*
-const long long int QUINTUS    = 83621143489848416212;
-const long long int SERENUS    = 298611126818977066911787;  //just took root squares of these
-const long long int SAMMONICUS = 354224848179261908310;   //numbers
-*/
+using namespace std;
+
 void findSumWays(int n, int arr[][2]) // even numbers only
 {
     int j = n;
@@ -38,11 +35,9 @@ int binarySearch(int first_number, int last_number) {
 
 //"int128" realization
 string add (string &s1, string &s2){
-    int carry=0,sum,i;
+    int carry=0,sum,i; //carry over numbers
 
-    string  min=s1,
-    max=s2,
-    result = "";
+    string  min=s1, max=s2, result = "";
 
     if (s1.length()>s2.length()){
         max = s1;
@@ -61,7 +56,7 @@ string add (string &s1, string &s2){
         result = (char)(sum + '0') + result;
     }
 
-    i = max.length() - min.length()-1;
+    i = max.length() - min.length()-1; //digit difference
 
     while (i>=0){
         sum = max[i] + carry - '0';
@@ -79,9 +74,43 @@ string add (string &s1, string &s2){
     return result;
 }
 
+string multiply(string &s1, string &s2) { //s2 as coef
+    int coefficient = stoi(s2);
+    string temp;
+    for (int i = 1; i < coefficient; i++) {
+        if (i == 1) { temp = add(s1, s1); }
+        else {
+            temp = add(temp, s1);
+        }
+    }
+    return temp;
+}
 
 
-void getting_sequence_number() {}    //wHat iF the sequence is greatest_secrete_code * Fi - 6765 ??1!/!/
+int fibonacci_numbers(string getting_sequence_number) {
+    int n = 150;  //n - number of fibonacci members in the array
+    string fn[n];
+    string f1 = "1";
+    string f2 = f1;
+    fn[0] = f1;
+    fn[1] = f2;
+    string sum;
+    for (int i = 2; i < n; i++) {
+        sum = add(f1, f2);
+        //cout << sum << endl;
+        fn[i] = sum;
+        f1 = f2;
+        f2 = sum;
+    }
+    // O(n)
+    for (int i = 0; i < n; i++) {
+        if (getting_sequence_number == fn[i]) {
+            return i;
+        }
+    }
+    return 0;
+}
+
 
 int main() {
     std::cout << "Hello! I'm ready for the your challenge." << '\n';
@@ -89,8 +118,25 @@ int main() {
     int n = binarySearch(1, 9999); //guessed number
     int all_sums[n/2][2];
     findSumWays(n, all_sums);
-    int greatest_secret_code = all_sums[1][0]; // just for example
+    // greater secret code = ...
 
+    string quintus = "83621143489848416212";
+    string sammonicus = "354224848179261908310";
+    string serenus = "298611126818977066911787";
+
+    string dif = "6765";
+    quintus = add(quintus, dif);
+    sammonicus = add(sammonicus, dif);
+    serenus = add(serenus, dif);
+
+    int seq_quintus = fibonacci_numbers(quintus) + 1;
+    int seq_sammonicus = fibonacci_numbers(sammonicus) + 1;
+    int seq_serenus = fibonacci_numbers(serenus) + 1; // * greatest code
+
+    cout << "It's your code phrase, get the pizza with it: " << endl <<  (char)seq_quintus << (char)(seq_quintus+1) << (char)seq_serenus
+        << (char)seq_quintus << (char)(seq_sammonicus-1) << (char)seq_quintus
+        << (char)seq_sammonicus << (char)seq_quintus << (char)(seq_quintus+1)
+        << (char)seq_serenus << (char)seq_quintus << endl;
 
     return 0;
 }
